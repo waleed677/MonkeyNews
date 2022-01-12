318c12184a23a6bb1ecc8335cce1973cd5917a65
Waleed Arshad Awan <waleedmalik677@gmail.com>
2022-01-12 22:40:07 +0500

    setup website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="container my-5">
+          <div className="row">
+            <div className="col-md-4">
+              <News title={"Code With Harry"} description={"Lorum Ispum"} />
+            </div>
+            <div className="col-md-4">
+              <News title={"Code With Harry"} description={"Lorum Ispum"} />
+            </div>
+            <div className="col-md-4">
+              <News title={"Code With Harry"} description={"Lorum Ispum"} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
