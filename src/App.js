bda9cc6b6906638cc7df04e56e9a8334d620a72f
Waleed Arshad Awan <waleedmalik677@gmail.com>
2022-01-15 23:13:10 +0500

    add infinite scroll and top bar loader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
+  state = {
+    progress: 0,
+  }
+
+  setProgress = (progress) => {
+    this.setState({progress: progress})
+  }
+
+
+  
   render() {
     return (
       <div>
         <Router>
+        <LoadingBar
+        color='#f11946'
+        height={3}
+        progress={this.state.progress}
+  
+      />
           <Navbar />
           <div className="container">
             <Routes>
-              <Route exact  path="/" element={<News key="general" category={"general"} />} />
-              <Route exact  path="/business" element={<News key="business" category={"business"} />}/>
-              <Route exact path="/entertainment" element={<News  key="entertainment" category={"entertainment"} />}/>
-              <Route exact  path="/health" element={<News  key="health" category={"health"} />} />
-              <Route exact  path="/science" element={<News key="science" category={"science"} />} />
-              <Route exact  path="/sports" element={<News key="sports" category={"sports"} />} />
-              <Route exact  path= "/technology" element={<News key="technology" category={"technology"} />} />
+              <Route exact  path="/" element={<News setProgress = {this.setProgress} key="general" category={"general"} />} />
+              <Route exact  path="/business" element={<News setProgress = {this.setProgress} key="business" category={"business"} />}/>
+              <Route exact path="/entertainment" element={<News setProgress = {this.setProgress}  key="entertainment" category={"entertainment"} />}/>
+              <Route exact  path="/health" element={<News setProgress = {this.setProgress}  key="health" category={"health"} />} />
+              <Route exact  path="/science" element={<News setProgress = {this.setProgress} key="science" category={"science"} />} />
+              <Route exact  path="/sports" element={<News setProgress = {this.setProgress} key="sports" category={"sports"} />} />
+              <Route exact  path= "/technology" element={<News setProgress = {this.setProgress} key="technology" category={"technology"} />} />
             </Routes>
           </div>
         </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
@@ -15,21 +16,22 @@ export class News extends Component {
       articles: [],
       loading: false,
       page: 1,
-      totalResuls: 0,
+      totalResults: 0,
     };
   }
 
   async updateState(page){
+    this.props.setProgress(20);
     let baseUrl = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=2675b76c6e714c869e7679ed95dacd8a&pageSize=${this.props.pageSize}&page=${page}`;
-    console.log(baseUrl);
     this.setState({ loading: true });
     let data = await fetch(baseUrl);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
-      totalResuls: parsedData.totalResults,
+      totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
@@ -45,31 +47,52 @@ export class News extends Component {
      this.updateState(this.state.page + 1);
   };
 
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    let baseUrl = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=2675b76c6e714c869e7679ed95dacd8a&pageSize=${this.props.pageSize}&page=${this.state.page}`;
+    console.log(baseUrl);
+    let data = await fetch(baseUrl);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  };
+
   render() {
     return (
       <div className="my-3">
-        <h2 className="text-center mb-5">NewsMonkey - Top Headlines</h2>
+        <h2 className="text-center mb-5">NewsMonkey - Top {this.props.category.toUpperCase()} Headlines</h2>
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title}
-                    description={element.description}
-                    imageUrl={element.urlToImage}
-                    newsUrl={element.url}
-                    author={element.author}
-                    date = {element.publishedAt}
-                    source = {element.source.name}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+        <div className="container my-5">
+            <div className="row">
+              {this.state.articles.map((element,index) => {
+                  return <div className="col-md-4" key={index}>
+                      <NewsItem
+                        title={element.title}
+                        description={element.description}
+                        imageUrl={element.urlToImage}
+                        newsUrl={element.url}
+                        author={element.author}
+                        date = {element.publishedAt}
+                        source = {element.source.name}
 
-                  />
+                      />
+                    </div>
+                  
+                })}
                 </div>
-              );
-            })}
-        </div>
-        <div className="container d-flex justify-content-evenly mt-4">
+            </div>
+        </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-evenly mt-4">
           <button
             type="button"
             className="btn btn-dark"
@@ -83,12 +106,12 @@ export class News extends Component {
             className="btn btn-dark"
             onClick={this.handleNextClick}
             disabled={
-              this.state.page + 1 >= Math.ceil(this.state.totalResuls / 10)
+              this.state.page + 1 >= Math.ceil(this.state.totalResutls / 10)
             }
           >
             Next
           </button>
-        </div>
+        </div> */}
       </div>
     );
   }
